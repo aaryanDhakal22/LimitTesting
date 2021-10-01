8ed29ce1383320e030ae5385e54031a1c412fd5f
Aaryan <47253380+aaryanDhakal22@users.noreply.github.com>
2021-10-01 22:22:10 +0545

    Finnnaly banyo candy curushu

--- LimitTest/Assets/Scripts/CandyCrush/objectcontrol.cs
+++ LimitTest/Assets/Scripts/CandyCrush/objectcontrol.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class objectcontrol : MonoBehaviour
 {
+    MeshRenderer rend;
+    public int i_cord = 0;
+    public int j_cord = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+           
+    }
+    private void OnMouseDown( )
+    {
+        transform.localScale = new Vector3(1.5f, 1.5f, 1);
+
         
+        if (!CandyCrush.one)
+        {
+            CandyCrush.one = gameObject;
+            CandyCrush.onemat = rend.material.color;
+            rend.material.color = Color.cyan;
+        }
+        else
+        {
+            CandyCrush.two = gameObject;
+            CandyCrush.twomat = rend.material.color;
+            rend.material.color = Color.cyan;
+        }
+
     }
 }
